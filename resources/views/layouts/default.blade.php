b80038896d061956b253d97bcdceb3a0353053fd
shixianhui <610197056@qq.com>
2020-11-03 11:28:46 +0800

    vscodepush

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title', 'weibo app')</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+@include('layouts._header')
+<div class="container">
+    @yield('content')
+    @include('layouts._footer')
+</div>
+</body>
+</html>
+
